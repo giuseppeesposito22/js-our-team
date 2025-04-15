7c29886278f386900e05e350eb3f77a8777fd74a
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-15 16:29:28 +0200

    aggiunto form per creare una nuova card

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 const rowCardEl = document.getElementById("row-card");
+const nameMemberInput = document.getElementById("name-member-input");
+const roleMemberInput = document.getElementById("role-member-input");
+const emailMemberInput = document.getElementById("email-member-input");
+const imgMemberInput = document.getElementById("img-member-input");
+const memberForm = document.getElementById("member-form");
 
 const teamMembers = [
   {
@@ -39,40 +44,65 @@ const teamMembers = [
   },
 ];
 
-for (const member of teamMembers) {
+/**
+ *
+ * Metodo per generare un elemento HTML passando un oggetto e andando ad inserire le sue proprietà all' interno
+ *
+ * @param {object} member passiamo un parametro di tipo oggetto
+ * @returns {string} ritorna una stringa
+ */
+const generateMemberCard = (member) => {
   const { name, role, email, img } = member;
 
-  let cardMember = `
+  const memberCard = `
   
-   <div class="col-sm-12 col-md-6 col-lg-4">
-            <div class="card mb-3 bg-dark text-white" style="max-width: 540px">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img
-                    id="image-member-card"
-                    src="./${img}"
-                    class="img-fluid"
-                    alt="..."
-                  />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 id="name-member-card" class="card-title fw-bold">
-                      ${name}
-                    </h5>
-                    <p id="role-member-card" class="card-text">
-                      ${role}
-                    </p>
-                    <p id="email-member-card" class="card-text text-primary">
-                      ${email}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-  
-  `;
+  <div class="col-sm-12 col-md-6  col-xl-4">
+           <div class="card mb-3 bg-dark text-white" style="max-width: 540px">
+             <div class="row g-0">
+               <div class="col-md-4">
+                 <img
+                   id="image-member-card"
+                   src="./${img}"
+                   class="img-fluid h-100 w-100"
+                   alt="..."
+                 />
+               </div>
+               <div class="col-md-8">
+                 <div class="card-body">
+                   <h5 id="name-member-card" class="card-title fw-bold">
+                     ${name}
+                   </h5>
+                   <p id="role-member-card" class="card-text">
+                     ${role}
+                   </p>
+                   <p id="email-member-card" class="card-text text-primary">
+                     ${email}
+                   </p>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+ 
+ `;
 
-  rowCardEl.innerHTML += cardMember;
+  return memberCard;
+};
+
+// Ciclo for of per generare le card all'interno della pagina utilizzando il metodo "generateMemberCard"
+for (const member of teamMembers) {
+  rowCardEl.innerHTML += generateMemberCard(member);
 }
+
+memberForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = nameMemberInput.value;
+  const role = roleMemberInput.value;
+  const email = emailMemberInput.value;
+  const img = imgMemberInput.value;
+
+  const newMember = { name, role, email, img };
+
+  rowCardEl.innerHTML += generateMemberCard(newMember);
+});
